0c0cabb18b7e92498479798954cf04e1d5d2ee36
keroseneyb <keroseneyb@gmail.com>
2025-08-09 17:16:31 +0500

    Move in presentation

--- app/src/main/java/com/kerosene/absolutecinema/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.kerosene.absolutecinema.ui.theme
+package com.kerosene.absolutecinema.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
