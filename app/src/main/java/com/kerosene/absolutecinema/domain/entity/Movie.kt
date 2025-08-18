c800b027f742a854265b5a1a9b8433e7da2a00fb
keroseneyb <keroseneyb@gmail.com>
2025-08-18 14:31:29 +0500

    Change entities

--- app/src/main/java/com/kerosene/absolutecinema/domain/entity/Movie.kt
+++ app/src/main/java/com/kerosene/absolutecinema/domain/entity/Movie.kt
@@ -1,13 +1,17 @@
 package com.kerosene.absolutecinema.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Movie(
     val id: Int,
-    val name: String,
+    val name: String?,
     val rating: Rating,
     val year: Int,
     val country: List<Country>,
     val genre: List<Genre>,
-    val description: String,
-    val shortDescription: String,
-    val poster: Poster,
-)
+    val description: String?,
+    val shortDescription: String?,
+    val poster: Poster?,
+): Parcelable
--- app/src/main/java/com/kerosene/absolutecinema/domain/entity/Poster.kt
+++ app/src/main/java/com/kerosene/absolutecinema/domain/entity/Poster.kt
@@ -1,5 +1,9 @@
 package com.kerosene.absolutecinema.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Poster(
-    val url: String,
-)
+    val url: String?,
+): Parcelable
