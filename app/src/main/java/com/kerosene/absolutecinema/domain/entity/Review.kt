abb50e45bfe4daae6f871ae27145797f9eb45fa7
keroseneyb <keroseneyb@gmail.com>
2025-08-21 22:07:02 +0500

    Change review type

--- app/src/main/java/com/kerosene/absolutecinema/domain/entity/Review.kt
+++ app/src/main/java/com/kerosene/absolutecinema/domain/entity/Review.kt
@@ -4,18 +4,6 @@ data class Review(
     val author: String,
     val title: String,
     val review: String,
-    val type: ReviewType
+    val type: ReviewType,
 )
 
-enum class ReviewType(val value: String) {
-
-    POSITIVE("Позитивный"),
-    NEGATIVE("Негативный"),
-    NEUTRAL("Нейтральный");
-
-    companion object {
-        fun fromValue(raw: String): ReviewType = entries.find {
-            it.value.equals(raw, ignoreCase = true)
-        } ?: NEUTRAL
-    }
-}
