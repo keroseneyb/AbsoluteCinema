5ec015926e22399407eb7881263da7c59e606d85
keroseneyb <keroseneyb@gmail.com>
2025-08-23 20:33:54 +0500

    Add ApiFactory to provide ApiService with OkHttpClient and Retrofit setup

---
+++ app/src/main/java/com/kerosene/absolutecinema/data/network/api/ApiFactory.kt
@@ -0,0 +1,35 @@
+package com.kerosene.absolutecinema.data.network.api
+
+import com.kerosene.absolutecinema.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object ApiFactory {
+
+    private const val KEY_PARAM = "token"
+    private const val BASE_URL = "https://api.kinopoisk.dev/v1.4/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val newUrl = originalRequest
+                .url
+                .newBuilder()
+                .addQueryParameter(KEY_PARAM, BuildConfig.CINEMA_API_KEY)
+                .build()
+            val newRequest = originalRequest.newBuilder()
+                .url(newUrl)
+                .build()
+            chain.proceed(newRequest)
+        }.build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    val apiService: ApiService = retrofit.create()
+}
