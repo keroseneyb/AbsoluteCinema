0f921c8416e61fdbe982f8d341b513eb79fe55ac
keroseneyb <keroseneyb@gmail.com>
2025-08-25 17:16:41 +0500

    Configure ViewModelModule with ViewModel bindings for DI

---
+++ app/src/main/java/com/kerosene/absolutecinema/data/di/ViewModelModule.kt
@@ -0,0 +1,40 @@
+package com.kerosene.absolutecinema.data.di
+
+import androidx.lifecycle.ViewModel
+import com.kerosene.absolutecinema.presentation.screens.details.MovieDetailsViewModel
+import com.kerosene.absolutecinema.presentation.screens.home.HomeViewModel
+import com.kerosene.absolutecinema.presentation.screens.library.LibraryViewModel
+import com.kerosene.absolutecinema.presentation.screens.library.NoteViewModel
+import com.kerosene.absolutecinema.presentation.screens.search.SearchViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MovieDetailsViewModel::class)
+    fun bindMovieDetailsViewModel(viewModel: MovieDetailsViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SearchViewModel::class)
+    fun bindSearchViewModel(viewModel: SearchViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LibraryViewModel::class)
+    fun bindLibraryViewModel(viewModel: LibraryViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    fun bindHomeViewModel(viewModel: HomeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(NoteViewModel::class)
+    fun bindNoteViewModel(viewModel: NoteViewModel): ViewModel
+}
