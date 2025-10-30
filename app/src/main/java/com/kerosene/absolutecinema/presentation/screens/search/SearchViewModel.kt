22e2091b65910b17c99c0065e0eb4a1d48dbaf42
keroseneyb <keroseneyb@gmail.com>
2025-10-30 07:13:24 +0500

    Add filter valids movies

--- app/src/main/java/com/kerosene/absolutecinema/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/screens/search/SearchViewModel.kt
@@ -2,6 +2,7 @@ package com.kerosene.absolutecinema.presentation.screens.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kerosene.absolutecinema.domain.entity.Movie
 import com.kerosene.absolutecinema.domain.usecase.SearchMovieUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,21 +22,42 @@ class SearchViewModel @Inject constructor(
 
     fun onQueryChange(newQuery: String) {
         _query.value = newQuery
+
+        if (newQuery.trim().isEmpty()) {
+            _uiState.value = SearchUiState.Initial
+        }
     }
 
     fun searchMovies() {
         val currentQuery = _query.value
         if (currentQuery.isBlank()) return
 
-        _uiState.value = SearchUiState.Loading
-
         viewModelScope.launch {
-            try {
-                val result = searchMovieUseCase(currentQuery)
-                _uiState.value = SearchUiState.Success(result)
-            } catch (e: Exception) {
-                _uiState.value = SearchUiState.Error(e.message ?: UNKNOWN_ERROR)
+            validMovies(currentQuery)
+        }
+    }
+
+    private suspend fun validMovies(query: String) {
+        _uiState.value = SearchUiState.Loading
+        try {
+            val allMovies = searchMovieUseCase(query)
+            val validMovies = filterValidMovies(allMovies)
+
+            _uiState.value = if (validMovies.isEmpty()) {
+                SearchUiState.Empty
+            } else {
+                SearchUiState.Success(validMovies)
             }
+        } catch (e: Exception) {
+            _uiState.value = SearchUiState.Error(e.message ?: UNKNOWN_ERROR)
+        }
+    }
+
+    private fun filterValidMovies(movies: List<Movie>): List<Movie> {
+        return movies.filter { movie ->
+            movie.name != null &&
+            movie.poster?.url != null &&
+            movie.description != null
         }
     }
 
