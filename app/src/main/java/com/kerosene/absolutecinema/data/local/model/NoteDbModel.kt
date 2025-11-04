e8adeafb2602836d2141eee011207d44053e89ef
keroseneyb <keroseneyb@gmail.com>
2025-11-04 10:58:28 +0500

    Remove useless id, rename noteId

--- app/src/main/java/com/kerosene/absolutecinema/data/local/model/NoteDbModel.kt
+++ app/src/main/java/com/kerosene/absolutecinema/data/local/model/NoteDbModel.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
 data class NoteDbModel(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val movieId: Int,
+    @PrimaryKey
+    val noteId: Int,
     val title: String,
     val content: String,
 )
