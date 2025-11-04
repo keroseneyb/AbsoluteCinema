bf71d5d47446ed6939f208ae40f1549e01e9f7bb
keroseneyb <keroseneyb@gmail.com>
2025-11-04 11:00:30 +0500

    Remove noteToDbModel, change map in entity

--- app/src/main/java/com/kerosene/absolutecinema/data/mapper/NoteMapper.kt
+++ app/src/main/java/com/kerosene/absolutecinema/data/mapper/NoteMapper.kt
@@ -4,15 +4,7 @@ import com.kerosene.absolutecinema.data.local.model.NoteDbModel
 import com.kerosene.absolutecinema.domain.entity.Note
 
 fun NoteDbModel.noteToEntity() = Note(
-    id = id,
-    movieId = movieId,
-    title = title,
-    content = content
-)
-
-fun Note.noteToDbModel() = NoteDbModel(
-    id = id,
-    movieId = movieId,
+    noteId = noteId,
     title = title,
     content = content
 )
