86cea12d925b34484707e1b706ffea67d0b15870
keroseneyb <keroseneyb@gmail.com>
2025-11-05 16:18:20 +0500

    Remove initial state

--- app/src/main/java/com/kerosene/absolutecinema/presentation/screens/details/MovieDetailsUiState.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/screens/details/MovieDetailsUiState.kt
@@ -1,20 +1,12 @@
 package com.kerosene.absolutecinema.presentation.screens.details
 
-import com.kerosene.absolutecinema.domain.entity.Movie
-import com.kerosene.absolutecinema.domain.entity.Review
-import com.kerosene.absolutecinema.domain.entity.Trailer
+import com.kerosene.absolutecinema.presentation.screens.details.model.MovieDetailsUiModel
 
 sealed class MovieDetailsUiState {
 
-    object Initial : MovieDetailsUiState()
-
     object Loading : MovieDetailsUiState()
 
-    data class Success(
-        val movie: Movie,
-        val trailers: List<Trailer>,
-        val reviews: List<Review>,
-    ) : MovieDetailsUiState()
+    data class Success(val movieDetails: MovieDetailsUiModel) : MovieDetailsUiState()
 
     data class Error(val message: String) : MovieDetailsUiState()
 }
