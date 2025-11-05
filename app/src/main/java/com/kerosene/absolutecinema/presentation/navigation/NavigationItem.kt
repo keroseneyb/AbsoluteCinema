1a90fbb7b800ac3e27dbe03f789a280c6e857fd7
keroseneyb <keroseneyb@gmail.com>
2025-11-05 16:14:34 +0500

    Remove useless parameter

--- app/src/main/java/com/kerosene/absolutecinema/presentation/navigation/NavigationItem.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/navigation/NavigationItem.kt
@@ -8,25 +8,21 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.kerosene.absolutecinema.R
 
 sealed class NavigationItem(
-    val screen: Screen,
     val titleResId: Int,
     val icon: ImageVector,
 ) {
 
     object Home : NavigationItem(
-        screen = Screen.Home,
         titleResId = R.string.navigation_item_home,
         icon = Icons.Outlined.Home
     )
 
     object Search : NavigationItem(
-        screen = Screen.Search,
         titleResId = R.string.navigation_item_search,
         icon = Icons.Outlined.Search
     )
 
     object Library : NavigationItem(
-        screen = Screen.Library,
         titleResId = R.string.navigation_item_library,
         icon = Icons.Outlined.LocalLibrary
     )
