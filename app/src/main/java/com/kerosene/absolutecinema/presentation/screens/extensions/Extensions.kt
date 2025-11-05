d8923d80c4e989c6f2cf5536e5ba7a9d57d83ba8
keroseneyb <keroseneyb@gmail.com>
2025-11-05 16:17:16 +0500

    Change directory

---
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/screens/extensions/Extensions.kt
@@ -0,0 +1,11 @@
+package com.kerosene.absolutecinema.presentation.screens.extensions
+
+import com.kerosene.absolutecinema.presentation.screens.home.HomeScreenUiState
+import com.kerosene.absolutecinema.presentation.screens.home.model.MoviePreviewUiModel
+
+fun HomeScreenUiState.Content.getMoviesForSelectedTab(): List<MoviePreviewUiModel> {
+    return when (selectedTab) {
+        HomeScreenUiState.Tab.POPULAR -> popularMovies
+        HomeScreenUiState.Tab.ALL -> allMovies
+    }
+}
