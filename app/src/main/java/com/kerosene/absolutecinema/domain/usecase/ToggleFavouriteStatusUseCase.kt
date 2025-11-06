5132fcbf4fd94efbeaca7de2284ad6eca9de8ca7
keroseneyb <keroseneyb@gmail.com>
2025-11-06 15:34:40 +0500

    Fix movieid + title

--- app/src/main/java/com/kerosene/absolutecinema/domain/usecase/ToggleFavouriteStatusUseCase.kt
+++ app/src/main/java/com/kerosene/absolutecinema/domain/usecase/ToggleFavouriteStatusUseCase.kt
@@ -1,6 +1,5 @@
 package com.kerosene.absolutecinema.domain.usecase
 
-import com.kerosene.absolutecinema.domain.entity.Movie
 import com.kerosene.absolutecinema.domain.repository.FavouriteRepository
 import com.kerosene.absolutecinema.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.first
@@ -10,15 +9,15 @@ class ToggleFavouriteStatusUseCase @Inject constructor(
     private val favouriteRepository: FavouriteRepository,
     private val noteRepository: NoteRepository,
 ) {
-    suspend operator fun invoke(movie: Movie) {
-        val isFavourite = favouriteRepository.observeIsFavourite(movie.id).first()
+    suspend operator fun invoke(movieId: Int, title: String) {
+        val isFavourite = favouriteRepository.observeIsFavourite(movieId).first()
 
         if (isFavourite) {
-            favouriteRepository.removeFromFavourite(movie.id)
-            noteRepository.removeNoteByMovieId(movie.id)
+            favouriteRepository.removeFromFavourite(movieId)
+            noteRepository.removeNoteByMovieId(movieId)
         } else {
-            favouriteRepository.addToFavourite(movie)
-            noteRepository.createEmptyNote(movie)
+            favouriteRepository.addToFavourite(movieId)
+            noteRepository.createEmptyNote(movieId, title)
         }
     }
 }
