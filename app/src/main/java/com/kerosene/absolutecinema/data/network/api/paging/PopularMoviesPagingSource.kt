c4072677bc2061039561c2b366c93205c60e4818
keroseneyb <keroseneyb@gmail.com>
2025-11-10 14:28:28 +0500

    Add paging sources for allmvoies and popular movies

---
+++ app/src/main/java/com/kerosene/absolutecinema/data/network/api/paging/PopularMoviesPagingSource.kt
@@ -0,0 +1,34 @@
+package com.kerosene.absolutecinema.data.network.api.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.kerosene.absolutecinema.data.network.api.ApiService
+import com.kerosene.absolutecinema.data.network.dto.MovieDto
+
+class PopularMoviesPagingSource(
+    private val apiService: ApiService,
+) : PagingSource<Int, MovieDto>() {
+
+    override fun getRefreshKey(state: PagingState<Int, MovieDto>): Int? {
+        return state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieDto> {
+        return try {
+            val page = params.key ?: 1
+            val limit = 30
+            val response = apiService.loadPagingPopularMovies(page = page, limit = limit).movies
+
+            val nextKey = if (response.isEmpty()) null else response.size.plus(page).plus(1)
+            val prevKey = if (page == 1) null else response.size.minus(limit)
+
+            LoadResult.Page(
+                data = response,
+                nextKey = nextKey,
+                prevKey = prevKey
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
