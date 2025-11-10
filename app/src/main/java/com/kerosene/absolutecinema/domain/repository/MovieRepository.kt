a3cf470f920314beb685d7ea0b7cb6da45b1525e
keroseneyb <keroseneyb@gmail.com>
2025-11-10 14:29:28 +0500

    Change paging movies

--- app/src/main/java/com/kerosene/absolutecinema/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/kerosene/absolutecinema/domain/repository/MovieRepository.kt
@@ -1,16 +1,18 @@
 package com.kerosene.absolutecinema.domain.repository
 
+import androidx.paging.PagingData
 import com.kerosene.absolutecinema.domain.entity.Movie
 import com.kerosene.absolutecinema.domain.entity.Review
 import com.kerosene.absolutecinema.domain.entity.Trailer
+import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
 
+    fun getMoviesPaged(): Flow<PagingData<Movie>>
+
+    fun getPopularMoviesPaged(): Flow<PagingData<Movie>>
+
     suspend fun getMovieDetails(movieId: Int): Movie
-
-    suspend fun getMovies(): List<Movie>
-
-    suspend fun getPopularMovies(): List<Movie>
 
     suspend fun getTrailers(movieId: Int): Result<List<Trailer>>
 
