106c8ad1ca3e9b8f40586d26a3d46f576f719275
keroseneyb <keroseneyb@gmail.com>
2025-12-01 11:18:29 +0500

    Add route screen

--- app/src/main/java/com/kerosene/absolutecinema/presentation/navigation/NavigationItem.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/navigation/NavigationItem.kt
@@ -8,21 +8,25 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.kerosene.absolutecinema.R
 
 sealed class NavigationItem(
+    val screen: Screen,
     val titleResId: Int,
     val icon: ImageVector,
 ) {
 
     object Home : NavigationItem(
+        screen = Screen.HomeGraph,
         titleResId = R.string.navigation_item_home,
         icon = Icons.Outlined.Home
     )
 
     object Search : NavigationItem(
+        screen = Screen.SearchGraph,
         titleResId = R.string.navigation_item_search,
         icon = Icons.Outlined.Search
     )
 
     object Library : NavigationItem(
+        screen = Screen.LibraryGraph,
         titleResId = R.string.navigation_item_library,
         icon = Icons.Outlined.LocalLibrary
     )
