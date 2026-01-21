bb16d630c1aee3cee1af0beabd1bf5678de4ddb0
keroseneyb <keroseneyb@gmail.com>
2026-01-22 02:51:35 +0500

    Add @AndroidEntryPoint to MainActivity

--- app/src/main/java/com/kerosene/absolutecinema/presentation/screens/main/MainActivity.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/screens/main/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.kerosene.absolutecinema.presentation.ui.theme.AbsoluteCinemaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
