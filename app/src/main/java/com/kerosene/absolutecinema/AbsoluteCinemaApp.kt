b2d2f236590c1bef6b6a762a7ab9bd3eed5d6297
keroseneyb <keroseneyb@gmail.com>
2026-01-22 02:42:31 +0500

    Remove applicationcomponent method for factory

--- app/src/main/java/com/kerosene/absolutecinema/AbsoluteCinemaApp.kt
+++ app/src/main/java/com/kerosene/absolutecinema/AbsoluteCinemaApp.kt
@@ -1,22 +1,7 @@
 package com.kerosene.absolutecinema
 
 import android.app.Application
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import com.kerosene.absolutecinema.data.di.ApplicationComponent
-import com.kerosene.absolutecinema.data.di.DaggerApplicationComponent
+import dagger.hilt.android.HiltAndroidApp
 
-class AbsoluteCinemaApp : Application() {
-
-    val component: ApplicationComponent by lazy {
-        DaggerApplicationComponent.factory().create(this)
-    }
-}
-
-@Composable
-fun getApplicationComponent(): ApplicationComponent {
-    val context = LocalContext.current.applicationContext
-    val app = context as? AbsoluteCinemaApp
-        ?: error("ApplicationContext is not AbsoluteCinemaApp")
-    return app.component
-}
+@HiltAndroidApp
+class AbsoluteCinemaApp : Application()
