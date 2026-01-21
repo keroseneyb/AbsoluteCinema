7944d06f49f93c6fe54453c04f23188f9430da9c
keroseneyb <keroseneyb@gmail.com>
2026-01-22 02:52:06 +0500

    Remove di logic from ui navigation

--- app/src/main/java/com/kerosene/absolutecinema/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/kerosene/absolutecinema/presentation/screens/main/MainScreen.kt
@@ -12,24 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.kerosene.absolutecinema.getApplicationComponent
 import com.kerosene.absolutecinema.presentation.navigation.AppNavGraph
 import com.kerosene.absolutecinema.presentation.navigation.NavigationItem
 import com.kerosene.absolutecinema.presentation.navigation.Screen
 import com.kerosene.absolutecinema.presentation.navigation.rememberNavigationState
 import com.kerosene.absolutecinema.presentation.screens.details.MovieDetailsScreen
-import com.kerosene.absolutecinema.presentation.screens.details.MovieDetailsViewModel
 import com.kerosene.absolutecinema.presentation.screens.home.HomeScreen
-import com.kerosene.absolutecinema.presentation.screens.home.HomeViewModel
 import com.kerosene.absolutecinema.presentation.screens.library.favourites.LibraryScreen
-import com.kerosene.absolutecinema.presentation.screens.library.favourites.LibraryViewModel
 import com.kerosene.absolutecinema.presentation.screens.library.notes.EditNoteScreen
-import com.kerosene.absolutecinema.presentation.screens.library.notes.NoteViewModel
 import com.kerosene.absolutecinema.presentation.screens.search.SearchScreen
-import com.kerosene.absolutecinema.presentation.screens.search.SearchViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,7 +33,6 @@ fun MainScreen() {
         NavigationItem.Search,
         NavigationItem.Library
     )
-    val component = getApplicationComponent()
     Scaffold(
         bottomBar = {
             NavigationBar {
@@ -67,9 +59,7 @@ fun MainScreen() {
             AppNavGraph(
                 navController = navigationState.navController,
                 homeScreenContent = {
-                    val viewModel: HomeViewModel = viewModel(factory = component.getViewModelFactory())
                     HomeScreen(
-                        viewModel = viewModel,
                         onSearchClick = {
                             navigationState.navigateTo(Screen.SearchGraph.route)
                         },
@@ -79,9 +69,7 @@ fun MainScreen() {
                     )
                 },
                 searchScreenContent = {
-                    val viewModel: SearchViewModel = viewModel(factory = component.getViewModelFactory())
                     SearchScreen(
-                        viewModel = viewModel,
                         onMovieClick = { movieId ->
                             navigationState.navigateToMovieDetails(
                                 movieId,
@@ -91,13 +79,10 @@ fun MainScreen() {
                         onBackClick = {
                             navigationState.navigateTo(Screen.HomeGraph.route)
                         },
-                        onQueryChange = { newQuery -> viewModel.onQueryChange(newQuery) }
                     )
                 },
                 libraryScreenContent = {
-                    val viewModel: LibraryViewModel = viewModel(factory = component.getViewModelFactory())
                     LibraryScreen(
-                        viewModel = viewModel,
                         onMovieClick = { movieId ->
                             navigationState.navigateToMovieDetails(
                                 movieId,
@@ -111,17 +96,13 @@ fun MainScreen() {
                 },
                 movieDetailsScreenContent = { movieIdString ->
                     val movieId = movieIdString.toInt()
-                    val viewModel: MovieDetailsViewModel = viewModel(factory = component.getViewModelFactory())
                     MovieDetailsScreen(
-                        viewModel = viewModel,
                         movieId = movieId,
                     )
                 },
                 editNoteScreenContent = { movieIdString ->
                     val movieId = movieIdString.toInt()
-                    val viewModel: NoteViewModel = viewModel(factory = component.getViewModelFactory())
                     EditNoteScreen(
-                        viewModel = viewModel,
                         movieId = movieId,
                         onBack = { navigationState.navController.popBackStack() },
                     )
